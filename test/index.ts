d717b49b2db5441e1cb16b571a25f49e22430e8a
Yuki Takagi <takagiy.4dev@gmail.com>
2024-11-04 01:08:28 +0900

    add parser

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,8 @@
 import test from "ava";
+import * as parser from "../src/parser";
 
 test("it works", (t) => {
   t.is(1 + 1, 2);
+  const program = parser.parse("struct Foo { a: i32, b: i32 }");
+  console.dir(program, { depth: null });
 });
